2b44b46453255f5223708cce8c52f89b03a86bb4
syedmehdiabbas <mehdi110abbas@gmail.com>
2022-07-02 19:31:02 +0530

    Selection dropdown component for file types

---
+++ src/Home/components/SelectFileType/index.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import ReactSelect from "react-select";
+import fileTypes from "./../../../data/fileTypes";
+
+const customStyles = {
+  control: (provided, state) => ({
+    ...provided,
+    width: 200,
+    borderRadius: "999px",
+    background: "#edf2f7",
+    fontSize: "14px",
+  }),
+};
+
+const customTheme = (theme) => ({
+  ...theme,
+  borderRadius: 0,
+  colors: {
+    ...theme.colors,
+    primary25: "#E9D8FD", // purple.100 for option hover
+    primary: "#B794F4", //purple.300 for select control outline
+  },
+});
+
+function SelectFileType({ value, handleSelect }) {
+  const options = fileTypes.map((f) => ({ value: f, label: f }));
+
+  return (
+    <ReactSelect
+      options={options}
+      placeholder="File Type"
+      value={value}
+      onChange={handleSelect}
+      styles={customStyles}
+      theme={customTheme}
+    />
+  );
+}
+
+export default SelectFileType;
