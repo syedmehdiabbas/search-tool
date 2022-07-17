e7388ed3048cccebcfd79b7c499eb1837a6c2697
syedmehdiabbas <mehdi110abbas@gmail.com>
2022-07-18 01:27:01 +0530

    gave gradient to the heading text

--- src/Home/components/Header/index.jsx
+++ src/Home/components/Header/index.jsx
@@ -1,12 +1,20 @@
 import React from "react";
-import { Box, Heading, Text } from "@chakra-ui/react";
+import { Box, Heading, HStack, Text } from "@chakra-ui/react";
 
 function Header() {
   return (
     <Box textAlign="center">
-      <Heading color="purple.900" textShadow="base">
-        Search Better 🦸🏽‍♂️
-      </Heading>
+      <HStack justify="center">
+        <Heading
+          bgGradient="linear(to-r, pink.500, purple.600)"
+          textColor="transparent"
+          bgClip="text"
+          textShadow="base"
+        >
+          Search Better
+        </Heading>
+        <Heading>🦸🏽‍♂️</Heading>
+      </HStack>
       <Text fontSize="large" mt={2} color="purple.900">
         Add superpowers to you google search!
       </Text>
