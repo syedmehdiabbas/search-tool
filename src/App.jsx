b974fb024c9c3fc25597bc4bf00e111b631d0e39
syedmehdiabbas <mehdi110abbas@gmail.com>
2022-07-18 01:26:44 +0530

    top border on app removed

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import Home from "./Home";
 function App() {
   return (
     <Container maxW="full" minH="100vh" bg="gray.50" px={0}>
-      <Box minH={1} bgGradient="linear(to-r, pink.400, purple.400)" w="full" />
       <Home />
     </Container>
   );
