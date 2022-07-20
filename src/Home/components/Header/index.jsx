f8a8d73af61af012eb676e7960ea7453c0199bfb
syedmehdiabbas <mehdi110abbas@gmail.com>
2022-07-20 18:20:32 +0530

    Changed title and added logo

--- src/Home/components/Header/index.jsx
+++ src/Home/components/Header/index.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Box, Heading, HStack, Text } from "@chakra-ui/react";
+import kungFu from "./../../../assets/kung-fu.png";
 
 function Header() {
   return (
@@ -10,13 +11,21 @@ function Header() {
           textColor="transparent"
           bgClip="text"
           textShadow="base"
+          _selection={{ background: "pink.400", color: "purple.50" }}
         >
-          Search Better
+          Search Fu
         </Heading>
-        <Heading>🦸🏽‍♂️</Heading>
+        <Heading>
+          <img src={kungFu} width="40" heoght="40" />
+        </Heading>
       </HStack>
-      <Text fontSize="large" mt={2} color="purple.900">
-        Add superpowers to you google search!
+      <Text
+        fontSize="large"
+        mt={2}
+        color="purple.900"
+        _selection={{ background: "purple.100" }}
+      >
+        Add superpowers to your search skills!
       </Text>
     </Box>
   );
