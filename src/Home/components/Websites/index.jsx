502908ecd114bade26102e1bc8169d066cd2132e
syedmehdiabbas <mehdi110abbas@gmail.com>
2022-07-21 00:07:56 +0530

    website pills layout fixes

--- src/Home/components/Websites/index.jsx
+++ src/Home/components/Websites/index.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import {
+  Box,
   Button,
   ButtonGroup,
   Flex,
@@ -12,7 +13,15 @@ import useStore from "../../../store";
 import DeleteWebsite from "./DeleteWebsite";
 
 const WebsitesList = ({ websites, handleWebsiteClick, websitesData }) => (
-  <>
+  <ButtonGroup
+    size="md"
+    colorScheme="purple"
+    variant="outline"
+    display="flex"
+    flexWrap="wrap"
+    gap={1}
+    justifyContent="center"
+  >
     {websites.map((website) => (
       <Button
         maxW="18ch"
@@ -26,43 +35,40 @@ const WebsitesList = ({ websites, handleWebsiteClick, websitesData }) => (
         {websitesData[website].name}
       </Button>
     ))}
-  </>
+  </ButtonGroup>
 );
 
 function Websites({ handleWebsiteClick }) {
   const websitesData = useStore((state) => state.websites);
   const allWebsites = Object.keys(websitesData);
   return (
-    <VStack spacing={5}>
+    <VStack spacing={3}>
       <Text
         size="md"
         color="purple.900"
+        align="center"
         _selection={{ background: "purple.100" }}
       >
         Select to get results from specific websites only
       </Text>
-      <ButtonGroup size="md" colorScheme="purple" variant="outline">
-        <Flex flexWrap="wrap" gap={3} justify="center">
-          <HStack>
-            {allWebsites.length !== 0 ? (
-              <WebsitesList
-                websites={allWebsites}
-                handleWebsiteClick={handleWebsiteClick}
-                websitesData={websitesData}
-              />
-            ) : (
-              <Text
-                color="purple.900"
-                _selection={{ background: "purple.100" }}
-              >
-                Add a website
-              </Text>
-            )}
-            <AddWebsite />
-            <DeleteWebsite />
-          </HStack>
-        </Flex>
-      </ButtonGroup>
+      <Box>
+        {allWebsites.length !== 0 ? (
+          <WebsitesList
+            websites={allWebsites}
+            handleWebsiteClick={handleWebsiteClick}
+            websitesData={websitesData}
+          />
+        ) : (
+          <Text color="purple.900" _selection={{ background: "purple.100" }}>
+            Add a website
+          </Text>
+        )}
+      </Box>
+
+      <HStack wrap="nowrap" gap={2} justify="center">
+        <AddWebsite />
+        <DeleteWebsite />
+      </HStack>
     </VStack>
   );
 }
